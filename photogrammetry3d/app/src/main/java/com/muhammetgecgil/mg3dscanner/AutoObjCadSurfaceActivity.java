16bed269e1e983aca7d553fcc7006c5c53373223
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-18 20:03:04 +0300

    Add forced OBJ auto viewer for generated surface

---
+++ photogrammetry3d/app/src/main/java/com/muhammetgecgil/mg3dscanner/AutoObjCadSurfaceActivity.java
@@ -0,0 +1,7 @@
+package com.muhammetgecgil.mg3dscanner;
+
+import android.net.Uri;
+
+public class AutoObjCadSurfaceActivity extends CadSurfaceActivity {
+ @Override String name(Uri u){ return "MG3D_GENERATED_SURFACE.obj"; }
+}
