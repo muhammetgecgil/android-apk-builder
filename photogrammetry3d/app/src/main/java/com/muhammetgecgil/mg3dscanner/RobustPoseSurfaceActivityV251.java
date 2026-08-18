82145424c6870ef4b82000c99c47c9067d30d539
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-18 19:50:54 +0300

    Auto-open CAD viewer after v2.5 surface generation

---
+++ photogrammetry3d/app/src/main/java/com/muhammetgecgil/mg3dscanner/RobustPoseSurfaceActivityV251.java
@@ -0,0 +1,19 @@
+package com.muhammetgecgil.mg3dscanner;
+
+import android.content.Intent;
+import android.net.Uri;
+
+public class RobustPoseSurfaceActivityV251 extends RobustPoseSurfaceActivityV25 {
+ @Override Uri save25(Mesh m) throws Exception {
+   Uri u = super.save25(m);
+   runOnUiThread(() -> {
+     try {
+       Intent i = new Intent(this, CadSurfaceActivity.class);
+       i.setData(u);
+       i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+       startActivity(i);
+     } catch (Throwable ignored) {}
+   });
+   return u;
+ }
+}
