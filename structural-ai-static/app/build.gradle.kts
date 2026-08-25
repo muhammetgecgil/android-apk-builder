ee80adfd847e5fbfcd4464dea8467d1b1c0f6d78
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-25 22:12:36 +0300

    Bump Structural AI Static to v0.8.0

--- structural-ai-static/app/build.gradle.kts
+++ structural-ai-static/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mg.structuralai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.7.0"
+        versionCode = 9
+        versionName = "0.8.0"
     }
 
     buildTypes {
