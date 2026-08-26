a5e50ca151b7648466eaaeac8c3569906f549958
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-26 12:55:33 +0300

    Structural AI: bump v1.3.2 auto alpha

--- structural-ai-static/app/build.gradle.kts
+++ structural-ai-static/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mg.structuralai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.3.1-auto-alpha"
+        versionCode = 16
+        versionName = "1.3.2-auto-alpha"
     }
 
     buildTypes {
