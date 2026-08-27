a34e36a55ae1801ebe47f3aba01fa31437ce1a67
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-27 23:43:36 +0300

    Version Sesli Rehber 0.11.0

--- sesli-rehber/app/build.gradle.kts
+++ sesli-rehber/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mgecgil.seslirehber"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.10.0"
+        versionCode = 11
+        versionName = "0.11.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
