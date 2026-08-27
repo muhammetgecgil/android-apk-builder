32756d986040f966948c4251521196afa5fd9f78
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-27 19:05:38 +0300

    Sesli Rehber v0.7: bump device-validation build

--- sesli-rehber/app/build.gradle.kts
+++ sesli-rehber/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mgecgil.seslirehber"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
