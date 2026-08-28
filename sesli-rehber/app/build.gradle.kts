8efa3e45f46a524b4ede58ea7ed9d0fcca61e907
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-29 01:11:46 +0300

    Bump Sesli Rehber to v0.14

--- sesli-rehber/app/build.gradle.kts
+++ sesli-rehber/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mgecgil.seslirehber"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.13.0"
+        versionCode = 14
+        versionName = "0.14.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
