1eb487456084ee569628562c1daa6fca0d90d541
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-29 13:21:25 +0300

    Sesli Rehber v0.21: test indoor Turkish object mappings

--- sesli-rehber/app/src/test/java/com/mgecgil/seslirehber/core/DistantLabelPolicyTest.java
+++ sesli-rehber/app/src/test/java/com/mgecgil/seslirehber/core/DistantLabelPolicyTest.java
@@ -13,6 +13,17 @@ public class DistantLabelPolicyTest {
         assertEquals("bariyer", DistantLabelPolicy.toTurkishObject("Road barrier"));
     }
 
+    @Test public void mapsIndoorObjectsUsedByCloseSemanticRecognizer() {
+        assertEquals("koltuk", DistantLabelPolicy.toTurkishObject("Sofa"));
+        assertEquals("koltuk", DistantLabelPolicy.toTurkishObject("Couch"));
+        assertEquals("sandalye", DistantLabelPolicy.toTurkishObject("Chair"));
+        assertEquals("yastık", DistantLabelPolicy.toTurkishObject("Pillow"));
+        assertEquals("halı", DistantLabelPolicy.toTurkishObject("Rug"));
+        assertEquals("dolap", DistantLabelPolicy.toTurkishObject("Cabinet"));
+        assertEquals("televizyon", DistantLabelPolicy.toTurkishObject("Television"));
+        assertEquals("saat", DistantLabelPolicy.toTurkishObject("Wall clock"));
+    }
+
     @Test public void groundSafetySemanticsAreNotClaimedByFarLabeler() {
         assertEquals("", DistantLabelPolicy.toTurkishObject("Staircase"));
         assertEquals("", DistantLabelPolicy.toTurkishObject("Pothole"));
