dbc180842392cd7fa1a8a82e4c09a28ab224e5e3
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-29 17:58:53 +0300

    Sesli Rehber v0.23: bump app version

--- sesli-rehber/app/build.gradle.kts
+++ sesli-rehber/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mgecgil.seslirehber"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.22.0"
+        versionCode = 23
+        versionName = "0.23.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
