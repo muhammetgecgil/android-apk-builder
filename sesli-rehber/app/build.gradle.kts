71070b6ba5f002976849cd332558f09ab4d7db14
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-29 13:52:48 +0300

    Sesli Rehber v0.22: bump version for broad context object intelligence

--- sesli-rehber/app/build.gradle.kts
+++ sesli-rehber/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mgecgil.seslirehber"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "0.21.0"
+        versionCode = 22
+        versionName = "0.22.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
