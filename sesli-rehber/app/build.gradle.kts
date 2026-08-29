84df17a16d9da41a7692baba1ee49dab80bc9142
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-29 12:54:43 +0300

    Sesli Rehber v0.20: bump camera-first HUD release

--- sesli-rehber/app/build.gradle.kts
+++ sesli-rehber/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mgecgil.seslirehber"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.19.0"
+        versionCode = 20
+        versionName = "0.20.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
