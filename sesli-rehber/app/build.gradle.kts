60d2f0f6cebe29b47dc8b979df5d43491dc10395
muhammetgecgil <muhammetgecgilapps@gmail.com>
2026-08-29 19:42:09 +0300

    Sesli Rehber v0.24: bump semantic fusion release

--- sesli-rehber/app/build.gradle.kts
+++ sesli-rehber/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mgecgil.seslirehber"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.23.0"
+        versionCode = 24
+        versionName = "0.24.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
